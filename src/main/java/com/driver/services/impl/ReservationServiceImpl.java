c299e3afe95dd97aad19efd4bc28d224ac709927
unknown <nikhilkalamdane99@gmail.com>
2023-01-28 17:47:29 +0530

    completed Reservation Controller and services

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -9,6 +9,7 @@ import com.driver.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,6 +24,56 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        User user = userRepository3.findById(userId).get();
+        ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
 
+        if(user == null || parkingLot == null){
+            return null;
+        }
+
+        Reservation reservation = new Reservation();
+
+        List<Spot> spotList = parkingLot.getSpotList();
+
+        Collections.sort(spotList, (s1, s2) -> s1.getPricePerHour() - s2.getPricePerHour());
+
+        boolean reserveSpot = false;
+
+        for(Spot spot : spotList){
+
+            int wheels = 0;
+            if(spot.getSpotType() == SpotType.TWO_WHEELER){
+                wheels = 2;
+            }else if(spot.getSpotType() == SpotType.FOUR_WHEELER){
+                wheels = 4;
+            }else {
+                wheels = Integer.MAX_VALUE;
+            }
+
+            if(numberOfWheels <= wheels && !spot.getOccupied()){
+                reserveSpot = true;
+
+                reservation.setSpot(spot);
+                reservation.setUser(user);
+                reservation.setNumberOfHours(timeInHours);
+
+                spot.setOccupied(true);
+                spot.getReservationList().add(reservation);
+
+                user.getReservationList().add(reservation);
+
+                userRepository3.save(user);
+                spotRepository3.save(spot);
+                reservationRepository3.save(reservation);
+
+                break;
+            }
+        }
+
+        if(reserveSpot == false){
+            throw new Exception("Reservation cannot be made!!!");
+        }
+
+        return reservation;
     }
 }
