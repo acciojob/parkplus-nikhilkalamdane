ae8ed25c3f33718868626ef1abbbf0dfbbffbdd2
unknown <nikhilkalamdane99@gmail.com>
2023-01-28 17:57:49 +0530

    completed Payment Controller and services

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -16,6 +18,25 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        Payment payment = reservation.getPayment();
 
+        int reservationPrice = reservation.getNumberOfHours() * reservation.getSpot().getPricePerHour();
+
+        try{
+            payment.setPaymentMode(PaymentMode.valueOf(mode.toUpperCase()));
+        }catch (Exception e){
+            throw new Exception("Payment mode not detected");
+        }
+
+        if(amountSent < reservationPrice){
+            throw new Exception("Insufficient Amount");
+        }
+
+        payment.setPaymentCompleted(true);
+        payment.setReservation(reservation);
+        paymentRepository2.save(payment);
+
+        return payment;
     }
 }
